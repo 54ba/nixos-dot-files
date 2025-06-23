206609144cfd1d7ab3381eddece7324c9480db43
NixOS System <root@mahmoud-laptop>
2025-06-23 16:38:18 +0000

    fix: update zsh configuration to use initContent and configure starship prompt

--- flake.nix
+++ flake.nix
@@ -31,6 +31,22 @@
                   theme = "robbyrussell";
                   plugins = [ "git" "docker" "python" ];
                 };
+                initContent = "eval \"$(starship init zsh)\"";
+              };
+              
+              programs.starship = {
+                enable = true;
+                enableZshIntegration = true;
+                settings = {
+                  add_newline = true;
+                  character = {
+                    success_symbol = "[➜](bold green)";
+                    error_symbol = "[✗](bold red)";
+                  };
+                  git_branch = {
+                    format = "[$symbol$branch]($style) ";
+                  };
+                };
               };
             };
           }
